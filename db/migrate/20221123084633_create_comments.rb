6198d1ffb2b4ca87a60825abef1171eef485fc5d
kahoxx121u <smileyxoxokaho1212@gmail.com>
2022-12-07 21:01:35 +0900

    修正

--- db/migrate/20221123084633_create_comments.rb
+++ db/migrate/20221123084633_create_comments.rb
@@ -1,13 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :prototype_id
       t.text         :content,     null: false
       t.references   :prototype,   null: false, foreign_key: true 
       t.references   :user,        null: false, foreign_key: true 
       t.timestamps
     end
   end
-end
 end
